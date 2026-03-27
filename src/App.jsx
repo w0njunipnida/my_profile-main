e73bcf4337b7958fd0d7e75af9c96714273e6035
w0njunipnida <xpbpq9981@gmail.com>
2026-03-27 12:38:41 +0900

    카드 호버 추가 및 테마 변경

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from '@gsap/react';
 
+
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 // ─── DATA ────────────────────────────────────────────────────────────────────
